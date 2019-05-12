59badce440fadd8ed045bee31533ab91f6ad81df
Oboo Chin <oboochin@gmail.com>
2019-05-12 13:08:24 +0800

    Initial Commit

---
+++ playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Oboo_chin on 2019/5/12.
+//  Copyright © 2019 Oboo_chin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
